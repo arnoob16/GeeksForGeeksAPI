2f6fdf1690c78412fdcd3a74ab98a1b6cf17aa62
Arnab <contact.arnab16@gmail.com>
2020-12-29 14:57:36 +0530

    Flask server made, API working fine

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import scrap
+
+app = Flask(__name__)
+api = Api(app)
+
+class geeksforgeeksAPI(Resource):
+    def get(self, username):
+        return scrap.fetchResponse(username)
+
+api.add_resource(geeksforgeeksAPI, "/<string:username>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
