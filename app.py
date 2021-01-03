dbf4b24fcf3c1752b3ee83c61608e2a2ad65d08f
Joshua Wright <40744027+Jwright707@users.noreply.github.com>
2021-01-03 12:47:10 -0800

    Changed vercel file for default "/"

--- app.py
+++ app.py
@@ -3,19 +3,14 @@ from flask_restful import Api, Resource
 import scrap
 
 app = Flask(__name__)
-
-@app.route("/")
-def default_redirect():
-    return redirect("http://github.com/arnoob16/GeeksForGeeksAPI/")
-
 api = Api(app)
 
-# class geeksforgeeksAPI(Resource):
-#     def get(self, username=""):
-#         return scrap.fetchResponse(username)
-#
-#
-# api.add_resource(geeksforgeeksAPI, "/<string:username>")
+class geeksforgeeksAPI(Resource):
+    def get(self, username=""):
+        return scrap.fetchResponse(username)
+
+
+api.add_resource(geeksforgeeksAPI, "/<string:username>")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
