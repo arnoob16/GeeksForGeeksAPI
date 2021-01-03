aa1e06b1680e56c91c192918063a19d279067b5d
Joshua Wright <40744027+Jwright707@users.noreply.github.com>
2021-01-03 12:39:23 -0800

    Testing redirect w/o other url

--- app.py
+++ app.py
@@ -5,17 +5,16 @@ import scrap
 app = Flask(__name__)
 api = Api(app)
 
-class geeksforgeeksAPI(Resource):
-    def get(self, username=""):
-        return scrap.fetchResponse(username)
-
-
-api.add_resource(geeksforgeeksAPI, "/<string:username>")
+# class geeksforgeeksAPI(Resource):
+#     def get(self, username=""):
+#         return scrap.fetchResponse(username)
+#
+#
+# api.add_resource(geeksforgeeksAPI, "/<string:username>")
 
 @app.route("/")
 def default_redirect():
-    url = "http://github.com/arnoob16/GeeksForGeeksAPI/"
-    return redirect(url, code=301)
+    return redirect("http://github.com/arnoob16/GeeksForGeeksAPI/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
